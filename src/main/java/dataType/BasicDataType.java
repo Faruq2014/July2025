82f12dd87d680b95a1a0ed85ae67f10c5f8c390a
Faruq2014 <mjmolla.com>
2025-09-21 09:26:49 -0400

    intial

---
+++ src/main/java/dataType/BasicDataType.java
@@ -0,0 +1,13 @@
+package dataType;
+
+public class BasicDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+		System.out.println("hello Faruq");
+		
+
+	}
+
+}
---
+++ src/main/java/dataType/NumericDataType.java
@@ -0,0 +1,45 @@
+package dataType;
+
+public class NumericDataType {
+
+	public static void main(String[] args) {
+	
+		// buying groceries from 100$ bill
+		
+		/*
+		 * 	declaration
+		 * int= data type
+		 * apple= variable name
+		 * = is operator
+		 * 2= is value 
+		 * ;= end of syntax
+		 */
+		
+		int apple=2;	
+		int milk=5;
+		int totalSpent=apple + milk; 
+		System.out.println(totalSpent);
+		//2+5=7= total spend
+		
+		
+		// buying merchandise
+		
+		double tv=1299.99;
+		double soundBox=500.50;
+		double totalMerchandise=tv+soundBox;
+		System.out.println(totalMerchandise);
+		
+		
+		// buying soccer
+		
+		int soccerBall=25;
+		double jersy=65.99;
+		double totalSoccer=soccerBall+jersy;
+		//when you have mix data type---> chose bigger data type
+		System.out.println(totalSoccer);
+		
+		
+
+	}
+
+}
